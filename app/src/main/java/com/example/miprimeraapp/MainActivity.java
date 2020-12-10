3d93b864eec94f1f7652eae270ca32c744dee5bb
Cristian Vidal <Cristian.vidal.lopez@gmail.com>
2020-12-10 12:57:25 -0300

    ADD: metodo y muestro en pantalla.

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,8 +15,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String name = "Criss";
-        Log.d("TAG", "ALGO");
+        TextView miTextView = findViewById(R.id.textViewMain);
 
+        int resultado = add("");
+        Log.d("RESULTADO1", String.valueOf(resultado));
+
+        resultado = add("3");
+        Log.d("RESULTADO2", String.valueOf(resultado));
+        miTextView.setText("El resultado es : " + resultado);
+
+        resultado = add("3,5");
+        Log.d("RESULTADO3", String.valueOf(resultado));
+
+
+        miTextView.setText("El resultado es : " + resultado);
+
+
+
+    }
+
+    private int add(String values) {
+        if (values.isEmpty()) {
+            return 0;
+        } else if (values.length() == 3){
+            String valuesArray[] = values.split(",");
+            int result = Integer.parseInt(valuesArray[0]) + Integer.parseInt(valuesArray[1]);
+            return result;
+        } else {
+            return Integer.parseInt(values);
+        }
     }
 }
